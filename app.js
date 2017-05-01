b78d0db3214d7bba80aba712c516d62275b1b7da
Daniel Deutsch <deudan1010@gmail.com>
2017-05-01 23:54:53 +0200

    log actual json data

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ const https = require('https');
 const url = "https://teamtreehouse.com/danieldeutsch3.json";
 
 https.get(url, (res) => {
-  console.dir(res.statusCode);
-
-//   res.on('data', (d) => {
-//     process.stdout.write(d);
-//   });
-
-// }).on('error', (e) => {
-//   console.error(e);
+    console.dir(res.statusCode);
+    let body="";
+    res.on('data', (d) => {
+        body += d.toString();
+    }).on('error', (e) => {
+        console.error(e);
+    }).on('end', () => {
+    const profile = JSON.parse(body);
+    printMessage(profile.name, profile.badges.length, profile.points.JavaScript);
+    });
 });
 
 
 function printMessage(userName, badge, points) {
-    const message= `${userName} has a ${badge} badge and ${points} points!`;
+    const message= `${userName} has ${badge} badges and ${points} points!`;
     console.log(message);
 }
-printMessage("Daniel", "JavaScript", 100);
