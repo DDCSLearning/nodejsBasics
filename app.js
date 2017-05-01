ca93bf529bef46440894231bf53ee08b2ee81a17
Daniel Deutsch <deudan1010@gmail.com>
2017-05-01 17:00:35 +0200

    add console log

---
+++ app.js
@@ -0,0 +1,5 @@
+function printMessage(userName, badge, points) {
+    const message= `${userName} has a ${badge} badge and ${points} points!`;
+    console.log(message);
+}
+printMessage("Daniel", "JavaScript", 100);
